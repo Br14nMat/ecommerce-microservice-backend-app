e30ff77f9ffb52cf3c429a90f326c0300b591db3
Br14nMat <1108638502@u.icesi.edu.co>
2025-05-29 17:37:50 -0500

    pre deploy

---
+++ locust/locustfile-user.py
@@ -0,0 +1,22 @@
+from locust import HttpUser, task, between
+import random
+
+class UserService(HttpUser):
+    wait_time = between(1, 3)
+
+    @task(2)
+    def get_all_users(self):
+        self.client.get("/api/users")
+
+    @task(2)
+    def get_all_address(self):
+        self.client.get("/api/address")
+
+    @task(2)
+    def get_all_credentials(self):
+        self.client.get("/api/credentials")
+
+    @task(2)
+    def get_all_tokens(self):
+        self.client.get("/api/tokens")
+    
